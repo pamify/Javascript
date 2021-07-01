85fbd2f6d42ff2211516908e681071b59e02b2c9
pamify <pamjung@gmail.com>
2021-07-01 14:48:44 -0400

    Refactor BubbleSort two ways

--- Sorts/BubbleSort.js
+++ Sorts/BubbleSort.js
@@ -30,65 +30,71 @@
 *  [1, 2, 3]
 */
 
-/*
-*  Using 2 for loops
-*/
-function bubbleSort (items) {
-  const length = items.length
 
-  for (let i = (length - 1); i > 0; i--) {
-    // Number of passes
-    for (let j = (length - i); j > 0; j--) {
-      // Compare the adjacent positions
-      if (items[j] < items[j - 1]) {
-        // Swap the numbers
-        [items[j], items[j - 1]] = [items[j - 1], items[j]]
+// I - unsorted array
+// O - sorted array
+// C - none
+// E - empty array, sorted array
+
+function bubbleSort(array) {
+  // loop over the input array, starting at index 0
+  for (var i = 0; i < array.length; i++) {
+    // loop a second time over the input array, starting at adjacent element 
+    for (var k = i+1; k < array.length; k++) {
+      // store the first element in a temp variable
+      var temp = array[i];
+      // if the first element is larger than the second element, we want to swap
+      if (array[i] > array[k]) {
+        // reassign the first element to be the second
+        array[i] = array[k];
+        // reassign the second element to be the value stored in temp
+        array[k] = temp;
       }
     }
   }
-
-  return items
+  // return the sorted array
+  return array;
 }
 
-/*
-*  Implementation of 2 for loops method
-*/
-const array1 = [5, 6, 7, 8, 1, 2, 12, 14]
-// Before Sort
-console.log('\n- Before Sort | Implementation using 2 for loops -')
-console.log(array1)
-// After Sort
-console.log('- After Sort | Implementation using 2 for loops -')
-console.log(bubbleSort(array1))
-console.log('\n')
+// console.log(bubbleSort([5, 4, 1, 2, 3]));
+// [1, 2, 3, 4, 5]
 
-/*
-*  Using a while loop and a for loop
-*/
-function alternativeBubbleSort (arr) {
-  let swapped = true
+// console.log(bubbleSort([]));
+// []
+// console.log(bubbleSort([1, 2, 3]));
+// [1, 2, 3]
 
+//======== Alternative Bubble Sort =============
+
+function alternativeBubbleSort(array) {
+  // create a swapped boolean flag and initialize to true
+  var swapped = true;
   while (swapped) {
-    swapped = false
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
-        swapped = true
+    // inside the while loop, reassign the flag to false
+    swapped = false;
+    // iterate with a for loop, starting at index 0 up to the last element
+    for (var i = 0; i < array.length - 1; i++) {
+      // store the value of the first element in a temp variable
+      var temp = array[i];
+      // determine if the elements need to be swapped
+      if (array[i] > array[i+1]) {
+        array[i] = array[i+1];
+        array[i+1] = temp;
+        // reassign the flag to be true after swapping
+        swapped = true;
       }
     }
   }
-
-  return arr
+  // return the sorted array
+  return array;
 }
 
-/*
-*  Implementation of a while loop and a for loop method
-*/
-const array2 = [5, 6, 7, 8, 1, 2, 12, 14]
-// Before Sort
-console.log('\n- Before Sort | Implementation using a while loop and a for loop -')
-console.log(array2)
-// After Sort
-console.log('- After Sort | Implementation using a while loop and a for loop -')
-console.log(alternativeBubbleSort(array2))
-console.log('\n')
+
+// console.log(alternativeBubbleSort([5, 4, 1, 2, 3]));
+// [1, 2, 3, 4, 5]
+
+// console.log(alternativeBubbleSort([]));
+// []
+
+// console.log(alternativeBubbleSort([1, 2, 3]));
+// [1, 2, 3]
